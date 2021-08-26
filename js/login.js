1d9732a06a201847c5bf06d3ddccd48f30d2a85d
Daph <daphnerodriguez169@gmail.com>
2021-08-26 10:58:31 -0300

    Login

--- js/login.js
+++ js/login.js
@@ -2,5 +2,21 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    e.preventDefault()
+    document.getElementById("log").addEventListener("click", function() { 
+
+    let Usser = "Hola.com" 
+    let Pass = 123456 
+    document.getElementById('user');
+    document.getElementById('password');
+    
+    console.log('Enviando formulario...');
+        
+    if(user.value=== Usser && password.value == Pass){
+        window.location='inicio.html'
+    }else{
+        alert('Su usuario o contraseña no son válidos');    
+    }
+   });
 
 });
