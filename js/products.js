a19d104b84aea21c8002d0bc78ad2e4487adb9ec
Daph <daphnerodriguez169@gmail.com>
2021-08-26 12:29:05 -0300

    Product list loaded

--- js/products.js
+++ js/products.js
@@ -1,6 +1,44 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+function LoadProdu(PRODUCTS_URL) {
+
+    document.getElementById("Pdt").innerHTML = " ";
+    
+    fetch(PRODUCTS_URL)
+    .then(req => req.json())
+    .then(LoadProdu => {
+        
+        LoadProdu.forEach(element => {
+        
+            let linea = "<br><hr><br>";
+        
+            let salto = "<br><br>";
+            
+            let list = " ";
+            list = `<ul>
+                <ol> <img src=" ` + element.imgSrc + ` " style="width:350px;">` + salto +`</ol>
+                <ol>` + `<strong>Marca: </strong>` + element.name +`</ol>
+                <ol>` + `<strong>Descripcion: </strong>`+ element.description + `</ol>
+                <ol>` + `<strong>Precio: </strong>` + element.cost + linea +`</ol>
+            </ul>`
+        document.getElementById("Pdt").innerHTML += list;
+        })
+    })
+    .catch(error => alert("Hay un error: " + error));
+}
+
+document.addEventListener("DOMContentLoaded", (e) =>{
+    let color = document.getElementById("Pdt");
+
+    color.addEventListener("mouseout", (e) =>{
+     
+        e.target.style.color = "purple";
+      
+        setTimeout(() =>{
+            e.target.style.color = "";
+      }, 1500);
+
+    }, false);
 
 });
