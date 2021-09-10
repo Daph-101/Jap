cbd97d0c29cb599a684d8eab483c1eecba455cf1
Daph <daphnerodriguez169@gmail.com>
2021-09-10 19:42:45 -0300

    .

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var autos={}
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(URL_C_Onix).then(function(mostrar){
+        if(mostrar.status === "ok"){
+            autos = mostrar.info
+            let nombre = document.getElementById("nombre") 
+            nombre.innerHTML = autos.name;
+        }
 
+    })
 });
