049f2ef9916a44b00c4ad9acb0d98ac55c9240e0
Daph <daphnerodriguez169@gmail.com>
2021-09-19 11:48:42 -0300

    .....

--- js/product-info.js
+++ js/product-info.js
@@ -2,7 +2,7 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-
+var newComen=[];
 function Comen(){
 
   fetch(PRODUCT_INFO_COMMENTS_URL)
@@ -12,27 +12,40 @@ function Comen(){
 
       let mostrar = "";
       mostrar= `<ul>
+      <hr>
         <p>` + `Fecha: ` + com.dateTime + `</p>
         <p>` + `Usuario: ` + com.user + `</p>
-        <p>` + `Descripción: `  + com.description + `</p>
-        <p>` + `Puntaje: `  + com.score + 
+        <p>` + `Descripción: ` + com.description + `</p>
+        <p>` + `Puntaje: `  + 
         `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">`+
         `<span class="fa fa-star checked"></span>
           <span class="fa fa-star checked"></span>
           <span class="fa fa-star checked"></span>
           <span class="fa fa-star"></span>
           <span class="fa fa-star"></span>
-        </div></p>
+        </div></p><br>
       </ul>`;
       document.getElementById("DatosCom").innerHTML += mostrar;
       
     })
-   
+  
   })
 
 }
-
-
+function NewComments() {
+  var nameField = document.getElementById('nameField').value;
+  var result = document.getElementById('result');
+  
+  if (nameField.length < 5) {
+      result.textContent = 'Tu comentario debe tener más de 5 caracteres';
+      //Avisa al usuario si el comentario tiene menos de 10 caracteres
+  } else {
+      result.textContent = '' + nameField;
+      //Muestra al usuario los datos que se guardaron en la variable "nameField"
+  }
+}
+var subButton = document.getElementById('subButton');
+subButton.addEventListener('click', NewComments, false); 
 
 
 var url;
@@ -51,31 +64,32 @@ document.addEventListener("DOMContentLoaded",
           auto= `<div>
           <p>` + `<strong>Marca: </strong>` + element.name +`</p>
           <p>` + `<strong>Descripcion: </strong>`+ element.description + `</p>
-          <p>` + `<strong>Precio: $</strong>` + element.cost + `</p>
-          
+          <p>` + `<strong>Precio: </strong> ` + element.currency + " " + element.cost + `</p>
+          <p><img src=" ` + element.images[0] + ` " style="width:350px;"></p>
+          <p><img src=" ` + element.images[1] + ` " style="width:350px;"></p>
+          <p><img src=" ` + element.images[2] + ` " style="width:350px;"></p>
           </div>`
           
     
           document.getElementById("informacion").innerHTML += auto;
         
         });
-        MostrarImg(url);
       })
 });
 
 
-function MostrarImg(url){
- url = URL_C_Onix;
+// function MostrarImg(url){
+//  url = URL_C_Onix;
   
-  let Img = "";
-  Img += `
+//   let Img = "";
+//   Img += `
  
-    <img src="img/car1.jpg" style="width:200px;">
-    <img src="img/cat1.jpg" style="width:200px;">
-    <img src="img/car3.jpg" style="width:200px;"><br><br>
-    <a href="products.html"><button id="info-auto">Volver a lista de autos</button></a> <br>
+//     <img src="img/car1.jpg" style="width:200px;">
+//     <img src="img/cat1.jpg" style="width:200px;">
+//     <img src="img/car3.jpg" style="width:200px;"><br><br>
+//     <a href="products.html"><button id="info-auto">Volver a lista de autos</button></a> <br>
  
-  `;
-  document.getElementById("imagenes").innerHTML = Img;
+//   `;
+//   document.getElementById("imagenes").innerHTML = Img;
   
-}
+// }
