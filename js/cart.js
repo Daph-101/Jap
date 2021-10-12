91796807c2da0e5654e8ea3281edba4dc274d115
Daph <daphnerodriguez169@gmail.com>
2021-10-12 08:57:08 -0300

    ll

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,16 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    
-     
+    //falta url de fetch()
+     function carritoDAta(carro){
+
+        console.log(data.articles[0].name)
+        let body="";
+         
+         for(let i = 0; i<carro.length; i++){
+             body+= ` <p>Hola:${data[i].articles}</p> `
+         }
+         document.getElementById(datos).innerHTML = body;
+     }
     
 });
