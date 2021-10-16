e071e8de7232d1d0f56680de53555f1b5152268d
Daph <daphnerodriguez169@gmail.com>
2021-10-16 13:05:46 -0300

    ..

--- js/cart.js
+++ js/cart.js
@@ -1,101 +1,47 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-// let articulos=[]
-
  document.addEventListener("DOMContentLoaded", function(e){
-    function mostrarCarro(){
     
+    function Comen(){
+
         fetch(CART_INFO_URL)
-        .then(response=>response.json())
-        .then(data => mostraD(data))
+        .then(req => req.json())
+        .then(art =>{
+        console.log(art.articles[0])
+        art.articles.forEach(item =>{
+  
+        let body = "";
+        body= `
+        <main id="info">
+          <div class="row ">
+            <div class="col-12 col-xl-6 col-md-6 p-2">
+              <div class="d-block mb-4 h-100">
+                <article class="card h-100 flex-shrink-0">
+                  <div class="card-body">
+                    <div class="d-flex mb-3">
+                      <img class="rounded float-start w-50 h-50 pt-3" src="${item.src}">
+                      <div class="p-2 w-100">
+                        <p class="h6 w-100">${item.name}</p>
+                        <p class="h6 w-100">Cantidad: ${item.count}</p>
+                        <p class="h6 w-100">Precio: ${item.currency} ${item.unitCost}</p>
+                        <button class="btn btn-outline-danger bd-highlight mt-4 btn-sm ">add to cart</button>
+                      </div>
+                    </div>
+                </article>
+              </div>
+            </div>
+          </div>` ;
+        document.getElementById("info").innerHTML += body;
+  
+        })
+  
+        })
         .catch(error => console.log(error))
-        
-        const mostraD = (data) =>{
-        
-            console.log(data.articles[0])
-            let body = '';
-        
-            for (let i = 0; i<data.length; i++){
-                let recorre = data[i]
-                
-                body +=`<p>Hola:${recorre.articles[0]}</p>`
-            }
-            document.getElementById('info').innerHTML = body
         }
-        
-        }
-        
-        mostrarCarro()
-     }
-   
+  
+        Comen()
     
-
-//     function carritoDAta(carro){
-
-    
-//     let body= "";
-
-//     for(let i = 0; i < carro.length; i++){
-//         let info = carro[i];
-
-//         console.log(data.articles[0].name)
-//         body+= `
-//         // <div class="col-lg-3 col-md-4 col-6">
-//         //     <div class="d-block mb-4 h-100">
-//                     <p>Hola:${info.articles[0]}</p>
-//         //          <img class="img-fluid img-thumbnail" src="${info.articles[0].src}" alt="">
-//         //     </div>
-//         // </div>
-//         // `
-
-//         document.getElementById("datos").innerHTML = body;
-//     }
-
-// carritoDAta();
-);
-
-// document.addEventListener("DOMContentLoaded", function(e){
-//     getJSONData(CART_INFO_URL).then(function(resultObj){
-//         if (resultObj.status === "ok"){
-//             carritoDAta()
-//         }
-//     });
-// // });
+})   
 
 
-// function carritoDAta(CART_INFO_URL) {
-   
-    
-//     fetch(CART_INFO_URL)
-//     .then(req => req.json())
-//     .then(carro => {
-//        console.log(hola)
-//         carro.forEach(element => {
-            
-//             let body = " ";
-
-//             body = `<div class="col-lg-3 col-md-4 col-6">
-//             <div class="d-block mb-4 h-100">
-//              <p>Hola:${element.articles[0].name}</p>
-//             <img class="img-fluid img-thumbnail" src="${element.articles[0].src}" alt="">
-//             </div>
-//             </div>`
-//             document.getElementById("datos").innerHTML += body;
-            
-//         })
-        
-//     })
-//     .catch(error => alert("Hay un error: " + error));
-// }
-
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(CART_INFO_URL)
-    .then(function(resultObj){
-        if (resultObj.status === "ok") {
-        cartprods = resultObj.data;
-        prods = cartprods.articles;
-        mostrarcarrito(prods);
-     }
- })
-});
