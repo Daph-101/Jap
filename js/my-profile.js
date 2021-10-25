046db3aefe5a735e0abb4b2904d39e56c033a5b5
Daph <daphnerodriguez169@gmail.com>
2021-10-25 12:54:00 -0300

    mm

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,35 +1,27 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
 
-document.getElementById("btn-enviar").addEventListener('click', function(){
+  document.getElementById("btn-enviar").addEventListener('click', function(){
+    let name = document.getElementById("name")
+    let age = document.getElementById("age")
+    let email = document.getElementById("email")
+    let tel = document.getElementById("tel")
     
-        let name = document.getElementById("name")
-        let age = document.getElementById("age")
-        let email = document.getElementById("email")
-        let tel = document.getElementById("tel")
-    
+    let datosPerfil ={
+      name: name.value,
+      age: age.value,
+      email: email.value,
+      tel: tel.value
+    }  
     if(datosPerfil){
-        localStorage.setItem('datosPerfil', JSON.stringify({nombre: name.value, email: email.value }));
-        console.log(datosPerfil, "estos son los datos")
+      localStorage.setItem('profile', JSON.stringify({user: datosPerfil}));
+      console.log(datosPerfil, "estos son los datos")
     }
-    
- })
+        
+  })
 
- document.addEventListener("DOMContentLoaded", function (e) {
-    let datos = localStorage.getItem('datosPerfil');
-    let infoUser = document.getElementById("info-user")
-    let user = document.getElementById("user");
-  
-    if (datos) {
-      datos = JSON.parse(datos);
-      user.innerText = user.innerText + datos.email;
-      infoUser.style = "display: inline-block";
-    }
-  
-    if (document.getElementById("btn-editar")) {
-      document.getElementById("btn-editar").addEventListener("click", function () {
-        localStorage.removeItem('datosPerfil');
-      })
-    }
-  });
+ 
+    
+});
